1b5bf1fadd2927e04c5e08d1757973e4cb67742a
Felipe Santos <felipesantos194@gmail.com>
2025-05-13 23:53:03 -0300

    modulo7

---
+++ Tarefas_Classes/Mesa.java
@@ -0,0 +1,13 @@
+public class Mesa {
+//Exercecio módulo 7 ebaac//
+
+    public static void main(String[] args) {
+
+        int codigo;
+
+        String preco;
+
+        String tamanho;
+    }
+
+}
