e6c1a34d7b83bb09f8d3f8966d36de03085798da
Felipe Santos <felipesantos194@gmail.com>
2025-05-14 20:00:11 -0300

    corrigindo atividade

---
+++ Tarefas_Classes/Lapis.java
@@ -0,0 +1,34 @@
+public class Lapis {
+
+    private int codigo;
+
+    private int preco;
+
+    private String cor;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public int getPreco() {
+        return preco;
+    }
+
+    public void setPreco(int preco) {
+        this.preco = preco;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+}
+
+
