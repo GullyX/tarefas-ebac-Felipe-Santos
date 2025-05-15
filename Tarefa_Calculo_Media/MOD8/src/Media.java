301190496d9d4eb53a1bd692623abc4ff725e5f1
Felipe Santos <felipesantos194@gmail.com>
2025-05-15 06:50:37 -0300

    tarefas modulo8

---
+++ Tarefa_Calculo_Media/MOD8/src/Media.java
@@ -0,0 +1,22 @@
+public class Media {
+
+
+    public static void main(String[] args) {
+        calculoMedia();
+    }
+            private static void calculoMedia () {
+
+
+            int nota1 = 10;
+            int nota2 = 8;
+            int nota3 = 6;
+            int nota4 = 9;
+            int media = nota1 + nota2 + nota3 + nota4;
+            media = media / 4;
+            System.out.println("A média é: " + media);
+
+        }
+        }
+
+
+
